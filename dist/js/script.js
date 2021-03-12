375e5e50e2f62f6613ceb569e11ea58281ceb74e
Ilya <iluha3211@gmail.com>
2021-03-12 04:08:55 +0200

    add modal window

--- dist/js/script.js
+++ dist/js/script.js
@@ -106,7 +106,6 @@ document.addEventListener("DOMContentLoaded", () => {
       content.classList.add('hide');
       content.classList.remove('show', 'fade');
     });
-    console.log(tabsContent);
     tabs.forEach(tab => tab.classList.remove('tabheader__item_active'));
   }
 
@@ -173,8 +172,80 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  setClock('.timer', deadline);
+  setClock('.timer', deadline); //Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]'),
+        modalTimerId = setTimeout(openModal, 5000, modal);
+
+  function openModal(modal) {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    modal.classList.add('fade');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+
+  function closeModal(modal) {
+    modal.classList.remove('show');
+    modal.classList.remove('fade');
+    modal.classList.add('hide');
+    document.body.style.overflow = '';
+  }
+
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', () => {
+      openModal(modal);
+    });
+  });
+  modalCloseBtn.addEventListener('click', () => closeModal(modal));
+  modal.addEventListener('click', e => {
+    if (e.target && e.target === modal) {
+      closeModal(modal);
+    }
+  });
+  document.addEventListener('keydown', e => {
+    if (modal.classList.contains('show') && e.code === "Escape") {
+      closeModal(modal);
+    }
+  });
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal(modal);
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
+'use strict'; // function User(name, id) {
+// 	this.name = name;
+// 	this.id = id;
+// 	this.human = true;
+// 	this.hello = function () {
+// 		console.log(`Hello ! ${this.name}`);
+// 	};
+// }
+
+
+function sayName(surname) {
+  console.log(this);
+  console.log(this.name + ' ' + surname);
+}
+
+const user = {
+  name: 'John'
+};
+sayName.call(user, 'Smith');
+sayName.apply(user, ['Smith']);
+
+function count(num) {
+  return this * num;
+}
+
+const double = count.bind(2);
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
 			content.classList.add('hide');
 			content.classList.remove('show', 'fade');
 		});
-		console.log(tabsContent);
 
 		tabs.forEach(tab => tab.classList.remove('tabheader__item_active'));
 	}
@@ -77,10 +76,94 @@ document.addEventListener("DOMContentLoaded", () => {
 			hours.innerHTML = setZero(time.hours);
 			minutes.innerHTML = setZero(time.minutes);
 			seconds.innerHTML = setZero(time.seconds);
-
 		}
 	}
 
 
 	setClock('.timer', deadline);
+
+	//Modal
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector('.modal'),
+		modalCloseBtn = document.querySelector('[data-close]'),
+		modalTimerId = setTimeout(openModal, 5000, modal);
+
+	function openModal(modal) {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		modal.classList.add('fade');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
+
+	function closeModal(modal) {
+		modal.classList.remove('show');
+		modal.classList.remove('fade');
+		modal.classList.add('hide');
+		document.body.style.overflow = '';
+	}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', () => {
+			openModal(modal);
+		});
+	});
+
+	modalCloseBtn.addEventListener('click', () => closeModal(modal));
+
+	modal.addEventListener('click', (e) => {
+		if (e.target && e.target === modal) {
+			closeModal(modal);
+		}
+	});
+
+	document.addEventListener('keydown', (e) => {
+		if (modal.classList.contains('show') && e.code === "Escape") {
+			closeModal(modal);
+		}
+	});
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal(modal);
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+
+	window.addEventListener('scroll', showModalByScroll);
+
+
+
+
 });
+
+
+'use strict';
+
+// function User(name, id) {
+// 	this.name = name;
+// 	this.id = id;
+// 	this.human = true;
+// 	this.hello = function () {
+// 		console.log(`Hello ! ${this.name}`);
+// 	};
+// }
+
+function sayName(surname) {
+	console.log(this);
+	console.log(this.name + ' ' + surname);
+}
+
+const user = {
+	name: 'John'
+};
+
+sayName.call(user, 'Smith');
+sayName.apply(user, ['Smith']);
+
+function count(num) {
+	return this * num;
+}
+
+const double = count.bind(2);
+
